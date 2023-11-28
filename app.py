691c4fc7560061fa075ee1bd89980da1767daf19
Victor <104403406+DeshiH@users.noreply.github.com>
2023-11-28 22:07:03 +0000

    Creation the game rock, paper, scissor with GitHub Copilot

--- app.py
+++ app.py
@@ -13,32 +13,40 @@ def hello():
 
 
 def game():
-        options = ["rock", "paper", "scissors"]
-        score = [0, 0]
+    options = ["rock", "paper", "scissors"]
+    score = [0, 0]
+
+    while True:
+        player1 = input("Player 1, choose rock, paper, or scissors: ").lower()
+
+        if player1 not in options:
+            print("Wrong option for Player 1. Please choose rock, paper, or scissors.")
+            continue
 
         while True:
-            player1 = input("Player 1, choose rock, paper o scissors: ")
-            player2 = input("Player 2, choose rock, paper o scissors: ")
+            player2 = input("Player 2, choose rock, paper, or scissors: ").lower()
 
-            if player1 not in options or player2 not in options:
-                print("Wrong option. Please, choose rock, paper o scissors.")
+            if player2 not in options:
+                print("Wrong option for Player 2. Please choose rock, paper, or scissors.")
                 continue
-
-            if player1 == player2:
-                print("Tie!")
-            elif (player1 == "rock" and player2 == "scissors") or (player1 == "paper" and player2 == "rock") or (player1 == "scissors" and player2 == "paper"):
-                print("Player 1 wins!")
-                score[0] += 1
             else:
-                print("Player 2 wins!")
-                score[1] += 1
-
-            option = input("¿ Want to play again ? (y/n): ")
-            if option.lower() != "y":
                 break
 
-        print("Final score:")
-        print("Player 1:", score[0])
-        print("Player 2:", score[1])
+        if player1 == player2:
+            print("Tie!")
+        elif (player1 == "rock" and player2 == "scissors") or (player1 == "paper" and player2 == "rock") or (player1 == "scissors" and player2 == "paper"):
+            print("Player 1 wins!")
+            score[0] += 1
+        else:
+            print("Player 2 wins!")
+            score[1] += 1
+
+        option = input("Do you want to play again? (y/n): ")
+        if option.lower() != "y":
+            break
+
+    print("Final score:")
+    print("Player 1:", score[0])
+    print("Player 2:", score[1])
 
 game()
